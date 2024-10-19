545bd87f961e40b49d3ecee19b79c7cf65e58376
brayancb <143953434+brayancb@users.noreply.github.com>
2024-10-19 17:23:27 -0300

    guard

--- src/http/guard/auth.guard.ts
+++ src/http/guard/auth.guard.ts
@@ -7,27 +7,43 @@ import { catchError, map } from 'rxjs/operators';
 export class AuthGuard implements CanActivate {
   constructor(private readonly httpService: HttpService) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    try {
-      const request = context.switchToHttp().getRequest();
-      const token = request.headers['authorization']?.split(' ')[1];
-  
-      const response = await lastValueFrom(
-        this.httpService.get(
-            
-          `${this.configService.get<string>('MS_IAM')}/check`,
-          
-          {
-            headers: { authorization: `Bearer ${token}` },
-          }
-        )
-      );
-  
-      console.log('Respuesta del check:', response.data); // Verifica la respuesta del microservicio
-      return true;
-    } catch (error) {
-      console.log('Error en AuthGuard:', error.message); // Imprime el error
-      throw new UnauthorizedException();
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorizationHeader = request.headers['authorization'];// es para obtener el token del header
+
+    // Verificar si el header Authorization está presente
+    if (!authorizationHeader) {
+      throw new UnauthorizedException('Authorization header missing');
     }
+
+    // Extraer el token del header 'Authorization'
+    const token = authorizationHeader.split(' ')[1]; // Formato: 'Bearer <token>'
+
+    if (!token) {
+      throw new UnauthorizedException('Token missing');
+    }
+
+    // Hacer la llamada HTTP al microservicio IAM para verificar el token
+    return this.httpService.post('http://192.168.0.18:3000/auth/check', {}, { // Enviar el token en los headers
+      headers: {
+        Authorization: `Bearer ${token}`, // Enviar el token en el header
+      },
+    }).pipe(
+      map(response => {
+        console.log(`Respuesta del servicio IAM: ${JSON.stringify(response.data)}`);
+
+        // Verificar si el estado es 201 y hay datos válidos en la respuesta
+        if (response.status === 201 && response.data) {
+          return true; // Token válido, permitir el acceso
+        }
+
+        // En caso de no ser válido, lanzar una excepción de no autorizado
+        throw new UnauthorizedException('Invalid token');
+      }),
+      catchError(error => {
+        console.error(`Error en la verificación del token: ${error.message}`);
+        throw new UnauthorizedException('Token verification failed');
+      })
+    );
   }
 }
